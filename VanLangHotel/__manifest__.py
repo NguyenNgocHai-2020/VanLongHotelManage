c5f7b052da48bcfcad92242f6c67e50489407cba
Haiminh <Haiminh@gmail.com>
2020-11-20 17:17:00 +0700

    update

--- VanLangHotel/__manifest__.py
+++ VanLangHotel/__manifest__.py
@@ -28,6 +28,7 @@
         'data/employee_data_default.xml',
         'data/customer_data_default.xml',
         'data/room_data_default.xml',
+        'data/booking_data_default.xml',
         'wizard/report_booking.xml',
         'report/create_paper_bill.xml',
         'report/return_booking_order.xml',
